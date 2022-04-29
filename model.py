d3a0194974d8c24a7a7647e5656405fa7dc5f66e
ZoomWang666 <hr_wang@whu.edu.cn>
2022-04-29 11:32:03 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch.nn.functional as F
 from utils import *
 from dataset import *
-from PEG_conv.peg_conv import PEGConv
+from PEG_layer.peg_conv import PEGConv
 from torch import nn
 class Net(torch.nn.Module):
     def __init__(self, in_feats_dim, pos_dim, hidden_dim, use_former_information = False):
